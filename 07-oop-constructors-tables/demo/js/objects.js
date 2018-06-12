174f0ff072d79d2a945ff9828a65916a712a718c
Keith Dahlby <keith@newbo.co>
2018-06-12 11:45:56 -0500

    Day 7 with prototypes!

--- 07-oop-constructors-tables/demo/js/objects.js
+++ 07-oop-constructors-tables/demo/js/objects.js
@@ -55,21 +55,36 @@ function Vehicle(make, model, year) {
 
   this.owners = [];
   this.serviceRecord = [];
-
-  this.honk = function() {
-    console.log(this.yearMakeModel() + ' says honk!');
-  };
-
-  this.yearMakeModel = function() {
-    return this.year + ' ' + this.make + ' ' + this.model;
-  };
 }
 
+Vehicle.prototype.honkMessage = 'honk';
+Vehicle.prototype.wheelCount = 4;
+
+Vehicle.prototype.honk = function() {
+  console.log(this.yearMakeModel() + ' says ' + this.honkMessage + ' on its ' + this.wheelCount + ' wheels!');
+};
+
+Vehicle.prototype.yearMakeModel = function() {
+  return this.year + ' ' + this.make + ' ' + this.model;
+};
 
 var leaf = new Vehicle('Nissan', 'Leaf', 2018);
 console.log(leaf);
 leaf.honk();
 console.log(leaf.yearMakeModel());
+
+leaf.honk = function() { console.log('Not your normal honk!'); }
+leaf.honk();
+
+var spyder = new Vehicle('Can-Am', 'Spyder', 2008);
+spyder.wheelCount = 3;
+spyder.honk();
+
+Vehicle.prototype.honkMessage = 'beep beep';
+spyder.honk();
+
+delete leaf.honk;
+leaf.honk();
 
 // Have to use new!
 // Vehicle('THis', 'won\'t', 'work');
