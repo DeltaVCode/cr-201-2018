f01ea42b8c5395e3b8f24ac34c1efa97553a69e9
Keith Dahlby <keith@newbo.co>
2018-06-19 09:52:27 -0500

    Day 11 refactor: extract getNextImage()

--- 11-av-practical-clicktracker/demo/js/vote.js
+++ 11-av-practical-clicktracker/demo/js/vote.js
@@ -1,16 +1,21 @@
 'use strict';
 
 var nextImage = 0;
+
+function getNextImage() {
+  var image = Placeholder.all[nextImage++];
+  console.log(image);
+  return image;
+}
+
 function displayImages() {
   // Display image1
-  var image1 = Placeholder.all[nextImage++];
-  console.log(image1);
+  var image1 = getNextImage();
   var img1 = document.getElementById('product-1');
   img1.src = image1.src;
   img1.currentPlaceholder = image1;
 
-  var image2 = Placeholder.all[nextImage++];
-  console.log(image2);
+  var image2 = getNextImage();
   var img2 = document.getElementById('product-2');
   img2.src = image2.src;
   img2.currentPlaceholder = image2;
