4549f6262f839c46aa3e3a86e826be442008c11c
Keith Dahlby <keith@newbo.co>
2018-06-19 10:25:27 -0500

    Show results after 25 clicks

--- 11-av-practical-clicktracker/demo/js/vote.js
+++ 11-av-practical-clicktracker/demo/js/vote.js
@@ -11,6 +11,7 @@ var voteCount = 0;
 function displayImages() {
   if (voteCount >= 25) {
     console.log('display results now!');
+    showResults();
     return;
   }
 
@@ -37,18 +38,36 @@ for(var i = 0; i < productImages.length; i++) {
   });
 }
 
-function Placeholder(name, src) {
+function Placeholder(name, src, testShowCount, testVoteCount) {
   this.name = name;
   this.src = src;
+  this.showCount = testShowCount || 0;
+  this.voteCount = testVoteCount || 0;
 
   Placeholder.all.push(this);
 }
 Placeholder.all = [];
 
-new Placeholder('placekitten.com', 'https://placekitten.com/g/150/150');
-new Placeholder('fillmurray.com', 'http://fillmurray.com/150/150');
-new Placeholder('placecage.com', 'http://placecage.com/150/150');
+new Placeholder('placekitten.com', 'https://placekitten.com/g/150/150', 7, 2);
+new Placeholder('fillmurray.com', 'http://fillmurray.com/150/150', 10, 10);
+new Placeholder('placecage.com', 'http://placecage.com/150/150', 6, 3);
 
 console.log('all Placeholders', Placeholder.all);
 
 window.addEventListener('load', displayImages);
+
+function showResults() {
+  var ul = document.getElementById('results');
+  // reset list
+  ul.innerHTML = '';
+
+  for(var i = 0; i < Placeholder.all.length; i++) {
+    var current = Placeholder.all[i];
+
+    var li = document.createElement('li');
+    li.textContent = current.name + ' got ' + current.voteCount + ' votes';
+    ul.appendChild(li);
+  }
+
+  // TODO: Add chart here
+}
