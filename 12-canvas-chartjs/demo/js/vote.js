7506260c1666850127ccf8a24893a06467742bdf
Keith Dahlby <keith@newbo.co>
2018-06-19 23:05:22 -0500

    Add comments to Day 12 demo

--- 12-canvas-chartjs/demo/js/vote.js
+++ 12-canvas-chartjs/demo/js/vote.js
@@ -1,6 +1,8 @@
 /* globals Chart */
 'use strict';
 
+// get the next image for display
+// TODO: randomize image order, without repeats
 function getNextImage() {
   var nextIndex = Math.floor(Math.random() * Placeholder.all.length);
   var image = Placeholder.all[nextIndex];
@@ -9,6 +11,7 @@ function getNextImage() {
 }
 
 var voteCount = 0;
+// display the next images
 function displayImages() {
   if (voteCount >= 5) {
     console.log('display results now!');
@@ -16,25 +19,36 @@ function displayImages() {
     return;
   }
 
+  // TODO: Can this use an array of the images?
+
   // Display image1
   var image1 = getNextImage();
   var img1 = document.getElementById('product-1');
   img1.src = image1.src;
-  img1.currentPlaceholder = image1;
 
+  // Let's save the current image so we can update its vote count
+  img1.currentPlaceholder = image1;
+  // TODO: track that image1 was shown
+
+  // Display image2
   var image2 = getNextImage();
   var img2 = document.getElementById('product-2');
   img2.src = image2.src;
   img2.currentPlaceholder = image2;
+  // TODO: track that image2 was shown
 }
 
 var productImages = document.querySelectorAll('#voting img');
 for(var i = 0; i < productImages.length; i++) {
   productImages[i].addEventListener('click', function (event) {
+    // Track click on overall vote count
     voteCount++;
 
-    console.log('click #' + voteCount, event.target.currentPlaceholder); 
+    console.log('click #' + voteCount, event.target.currentPlaceholder);
+    // TODO: track that currentPlaceholder received a vote
+    event.target.currentPlaceholder.voteCount ++;
 
+    // After vote, replace images for new vote
     displayImages();
   });
 }
@@ -42,18 +56,23 @@ for(var i = 0; i < productImages.length; i++) {
 function Placeholder(name, src, testShowCount, testVoteCount) {
   this.name = name;
   this.src = src;
+
+  // If testShowCount was not provided, use 0 instead
   this.showCount = testShowCount || 0;
+  // If testVoteCount was not provided, use 0 instead
   this.voteCount = testVoteCount || 0;
 
+  // Add this instance to our catalog of all Placeholders
   Placeholder.all.push(this);
 }
 Placeholder.all = [];
 
+// TODO: setup should probably live in a function
 new Placeholder('placekitten.com', 'https://placekitten.com/g/150/150', 7, 2);
 new Placeholder('fillmurray.com', 'http://fillmurray.com/150/150', 10, 10);
 new Placeholder('placecage.com', 'http://placecage.com/150/150', 6, 3);
 
-// Add temporary random vote/show counts
+// TEMP: Add random vote/show counts so chart is interesting
 for (var i = 0; i < Placeholder.all.length; i++) {
   Placeholder.all[i].voteCount = Math.floor(5 + Math.random() * 500);
   Placeholder.all[i].showCount = Math.floor(20 + Math.random() * 1000);
@@ -61,76 +80,113 @@ for (var i = 0; i < Placeholder.all.length; i++) {
 
 console.log('all Placeholders', Placeholder.all);
 
+// Display first round of images when page has loaded
 window.addEventListener('load', displayImages);
 
+// Show current results
 function showResults() {
   var ul = document.getElementById('results');
   // reset list
   ul.innerHTML = '';
 
+  // For each placeholder image...
   for(var i = 0; i < Placeholder.all.length; i++) {
     var current = Placeholder.all[i];
 
+    // Add it to <ul id="results">
     var li = document.createElement('li');
     li.textContent = current.name + ' got ' + current.voteCount + ' votes';
     ul.appendChild(li);
   }
 
+  // Also, show chart with current results
   showResultChart();
 }
 
 function showResultChart() {
+  // Chart must attach to a Canvas
   var canvas = document.getElementById('resultsCanvas');
 
-  // Un-hide our Canvas
+  // Un-hide our chart Canvas
   canvas.style.display = 'block';
 
+  // Arrays for our different data sets
+  // Will have one entry per Placeholder;
+  // all arrays should be the same length
   var labels = [];
   var voteCounts = [];
   var showCounts = [];
   var votePercentage = [];
+
+  // For each Placeholder instance...
   for (var i = 0; i < Placeholder.all.length; i++) {
     labels[i] = Placeholder.all[i].name;
     voteCounts[i] = Placeholder.all[i].voteCount;
     showCounts[i] = Placeholder.all[i].showCount;
+
+    // Calculate percentage of times image got votes out of all times shown
     votePercentage[i] = 100 * voteCounts[i] / showCounts[i];
   }
 
+  // Chart operates on the Canvas's 2d context
   var ctx = canvas.getContext('2d');
-  var chart = new Chart(ctx, {
+
+  // Create a new Chart linked to our Canvas
+  new Chart(ctx, {
+    // bar chart: http://www.chartjs.org/docs/latest/charts/bar.html
     type: 'bar',
+
+    // Chart data uses the same-sized arrays created above
     data: {
-      labels: labels,
+      labels: labels, // product names
       datasets: [
         {
           label: 'Vote Count',
           backgroundColor: 'rgb(200,0,0,0.6)',
-          data: voteCounts
+          data: voteCounts // product vote counts
         },
         {
           label: 'Show Count',
           backgroundColor: 'rgb(0,0,200,0.4)',
-          data: showCounts
+          data: showCounts // product shown counts
         },
         {
           label: 'Vote %',
-          data: votePercentage
+          data: votePercentage // vote percentage of times shown
         }
       ]
     },
     options: {
+      // automatically redraw chart when canvas is resized
+      // http://www.chartjs.org/docs/latest/general/responsive.html
       responsive: true,
+
+      // http://www.chartjs.org/docs/latest/axes/cartesian/
       scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: true
+        // Our primary X-axis is categories
+        // http://www.chartjs.org/docs/latest/axes/cartesian/category.html
+        // xAxes: [...] // default xAxes work fine, so no config necessary
+
+        yAxes: [
+          // Our primary Y-axis is linear
+          // http://www.chartjs.org/docs/latest/axes/cartesian/linear.html
+          {
+            ticks: {
+              beginAtZero: true
+            }
           }
-        }]
+          // TODO: secondary Y-axis for Vote %
+        ]
       },
+
+      // http://www.chartjs.org/docs/latest/configuration/title.html
       title: {
         display: true,
         text: 'Voting Results'
       }
+
+      // Lots of other configuration options could go here...
+      // http://www.chartjs.org/docs/latest/configuration/
     }
   });
 }
